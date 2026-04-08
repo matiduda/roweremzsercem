6e689cfe62d0ed97f1d74650c8258f464f56456d
Mateusz Duda <mateusz.duda2@gmail.com>
2026-04-08 22:50:39 +0200

    update

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -11,7 +11,7 @@ type Time = {
 
 export default function Countdown() {
   // 1 maja 2026 - 9:00 (UTC)
-  const targetDate = new Date("2026-04-08T22:37:00+02:00");
+  const targetDate = new Date("2026-05-01T09:00:00+02:00");
 
   const calculateTimeLeft = (): Time => {
     const difference = targetDate.getTime() - new Date().getTime();
